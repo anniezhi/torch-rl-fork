958ca8c3bc24e85ec27d8ff10ca4221ea976149a
Xiaoying Zhi <anniezhi97@gmail.com>
2022-12-20 15:45:44 +0100

    allow return agent loc and dir from step

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -80,7 +80,7 @@ for episode in range(args.episodes):
             frames.append(numpy.moveaxis(env.get_frame(), 2, 0))
 
         action = agent.get_action(obs)
-        obs, reward, terminated, truncated, _ = env.step(action)
+        obs, reward, terminated, truncated, _, _, _ = env.step(action)
         done = terminated | truncated
         agent.analyze_feedback(reward, done)
 
